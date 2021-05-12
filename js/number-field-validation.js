64b36ce925cdea614ca3f96a8eb7391da70cba10
xonism <gabriele.minc@protonmail.com>
2021-05-12 12:58:39 +0300

    added error check for number field value (above or equal to 1)

--- js/number-field-validation.js
+++ js/number-field-validation.js
@@ -1,6 +1,7 @@
 import { domElements } from './dom-elements.js';
 
 const MAX_CHARACTERS = 4;
+const MIN_VALUE = 1;
 const ERROR_MESSAGE_TIME_MS = 3000;
 
 const errorContainer$ = domElements.containers.error;
@@ -8,14 +9,31 @@ const numberField$ = domElements.formElements.stimulusNumberField;
 const errorMessage$ = domElements.errorMessage;
 const errorSound$ = domElements.errorSound;
 
+const isNumberFieldAboveCharacterLimit = () => numberField$.value.length > MAX_CHARACTERS;
+const isNumberFieldBelowAllowedValue = () => numberField$.value < MIN_VALUE;
+
 const limitFieldLength = (field$) => field$.value = field$.value.substr(0, MAX_CHARACTERS);
 const setErrorMessage = (text) => errorMessage$.innerHTML = text;
 const addNoVisibilityClassToErrorContainer = () => errorContainer$.classList.add('no-visibility');
 const removeNoVisibilityClassFromErrorContainer = () => errorContainer$.classList.remove('no-visibility');
-const isNumberFieldAboveCharacterLimit = () => numberField$.value.length > MAX_CHARACTERS;
 const playErrorSound = () => errorSound$.play();
+const setFieldValue = (field$) => field$.value = MIN_VALUE;
 
-const addNumberFieldCharacterLimitListeners = (chosenEvent) => {
+const addNumberFieldAllowedValueListener = (chosenEvent) => {
+    document.body.addEventListener(chosenEvent, function () {
+        if ( !isNumberFieldBelowAllowedValue() ) return;
+        
+        setFieldValue(numberField$);
+
+        setErrorMessage(`Quantity field can only contain values above or equal to ${MIN_VALUE}`);
+        playErrorSound();
+        removeNoVisibilityClassFromErrorContainer();
+
+        setTimeout(addNoVisibilityClassToErrorContainer, ERROR_MESSAGE_TIME_MS);
+    }, true);
+};
+
+const addNumberFieldCharacterLimitListener = (chosenEvent) => {
     document.body.addEventListener(chosenEvent, function () {
         if ( !isNumberFieldAboveCharacterLimit() ) return;
         
@@ -27,7 +45,10 @@ const addNumberFieldCharacterLimitListeners = (chosenEvent) => {
 
         setTimeout(addNoVisibilityClassToErrorContainer, ERROR_MESSAGE_TIME_MS);
     }, true);
-}
+};
 
-addNumberFieldCharacterLimitListeners("mouseup");
-addNumberFieldCharacterLimitListeners("keyup");
+addNumberFieldCharacterLimitListener("mouseup");
+addNumberFieldCharacterLimitListener("keyup");
+
+addNumberFieldAllowedValueListener("mouseup");
+addNumberFieldAllowedValueListener("keyup");
