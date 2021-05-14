e5af605603f11dac430011db85807650cbe51c2d
xonism <gabriele.minc@protonmail.com>
2021-05-14 14:16:27 +0300

    added error message fade out animation

--- js/number-field-validation.js
+++ js/number-field-validation.js
@@ -6,46 +6,49 @@ const ERROR_MESSAGE_TIME_MS = 3000;
 
 const errorContainer$ = domElements.containers.error;
 const numberField$ = domElements.formElements.stimulusNumberField;
-const errorMessage$ = domElements.errorMessage;
-const errorSound$ = domElements.errorSound;
 
 const isNumberFieldAboveCharacterLimit = () => numberField$.value.length > MAX_CHARACTERS;
 const isNumberFieldBelowAllowedValue = () => numberField$.value < MIN_VALUE;
 
 const setFieldValue = (field$) => field$.value = MIN_VALUE;
-const setErrorMessage = (text) => errorMessage$.innerHTML = text;
+const setErrorMessage = (text) => domElements.errorMessage.innerHTML = text;
 
-const addNoVisibilityClassToErrorContainer = () => errorContainer$.classList.add('no-visibility');
-const removeNoVisibilityClassFromErrorContainer = () => errorContainer$.classList.remove('no-visibility');
+const addErrorContainerNoVisibilityClass = () => errorContainer$.classList.add('no-visibility');
+const removeErrorContainerNoVisibilityClass = () => errorContainer$.classList.remove('no-visibility');
 
 const limitFieldLength = (field$) => field$.value = field$.value.substr(0, MAX_CHARACTERS);
-const playErrorSound = () => errorSound$.play();
+const playErrorSound = () => domElements.errorSound.play();
+
+const addErrorContainerFadeOutAnimation = () => errorContainer$.classList.add('fade-out-animation');
+const removeErrorContainerFadeOutAnimation = () => errorContainer$.classList.remove('fade-out-animation');
+
+function addErrorContainerFadeOutAndNoVisibility () {
+    addErrorContainerFadeOutAnimation();
+    setTimeout(removeErrorContainerFadeOutAnimation, 475);
+    setTimeout(addErrorContainerNoVisibilityClass, 475);
+}
 
 const addNumberFieldAllowedValueListener = (chosenEvent) => {
     document.body.addEventListener(chosenEvent, function () {
         if ( !isNumberFieldBelowAllowedValue() ) return;
-        
         setFieldValue(numberField$);
-
         setErrorMessage(`Quantity field can only contain values above or equal to ${MIN_VALUE}`);
-        playErrorSound();
-        removeNoVisibilityClassFromErrorContainer();
 
-        setTimeout(addNoVisibilityClassToErrorContainer, ERROR_MESSAGE_TIME_MS);
+        playErrorSound();
+        removeErrorContainerNoVisibilityClass();
+        setTimeout(addErrorContainerFadeOutAndNoVisibility, ERROR_MESSAGE_TIME_MS);
     }, true);
 };
 
 const addNumberFieldCharacterLimitListener = (chosenEvent) => {
     document.body.addEventListener(chosenEvent, function () {
         if ( !isNumberFieldAboveCharacterLimit() ) return;
-        
         limitFieldLength(numberField$);
-
         setErrorMessage(`Quantity field can only contain ${MAX_CHARACTERS} characters`);
-        playErrorSound();
-        removeNoVisibilityClassFromErrorContainer();
 
-        setTimeout(addNoVisibilityClassToErrorContainer, ERROR_MESSAGE_TIME_MS);
+        playErrorSound();
+        removeErrorContainerNoVisibilityClass();
+        setTimeout(addErrorContainerFadeOutAndNoVisibility, ERROR_MESSAGE_TIME_MS);
     }, true);
 };
 
